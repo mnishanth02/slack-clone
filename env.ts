80a927015db472acc9cba311d77116e796f4d463
Nishanth Murugan <nishanth.murugan@gmail.com>
2024-08-30 18:42:31 +0530

    adding conex

--- env.ts
+++ env.ts
@@ -1,22 +1,21 @@
-import { createEnv } from '@t3-oss/env-nextjs';
-import { config } from 'dotenv';
-import { z } from 'zod';
+import { createEnv } from "@t3-oss/env-nextjs";
+import { config } from "dotenv";
+import { z } from "zod";
 
 config();
 export const env = createEnv({
   server: {
-    NODE_ENV: z
-      .enum(['development', 'test', 'production'])
-      .default('development'),
+    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
     DATABASE_URL: z.string().url(),
-    GOOGLE_CLIENT_ID: z.string().min(1),
-    GOOGLE_CLIENT_SECRET: z.string().min(1),
+    CONVEX_DEPLOYMENT: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_CONVEX_URL: z.string().min(1),
   },
 
   experimental__runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
   },
 });
--- providers/providers.tsx
+++ providers/providers.tsx
@@ -4,16 +4,22 @@ import { ReactNode } from "react";
 import { ThemeProvider } from "@common/theme-provider";
 import { Toaster } from "@ui/sonner";
 import { TooltipProvider } from "@ui/tooltip";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import NextTopLoader from "nextjs-toploader";
 
+import { env } from "@/env";
 import QueryProvider from "./QueryProvider";
+
+const convex = new ConvexReactClient(env.NEXT_PUBLIC_CONVEX_URL as string);
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryProvider>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
         <NextTopLoader color="#15803d" shadow="0 0 10px #15803d,0 0 5px #15803d" />
-        <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+        <TooltipProvider delayDuration={0}>
+          <ConvexProvider client={convex}>{children}</ConvexProvider>
+        </TooltipProvider>
         <Toaster position="bottom-right" richColors duration={3000} toastOptions={{ style: { textAlign: "center" } }} />
       </ThemeProvider>
     </QueryProvider>
